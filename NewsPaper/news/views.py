1dd1bb805f0194ac67271d02ec1a2f8f1fb26532
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-05-05 22:27:45 +0300

    st4.0 mod24.6

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -4,9 +4,14 @@ from .models import Post
 
 class NewsList(ListView):
     model = Post
-    ordering = 'title'
+    ordering = '-post_time'
     template_name = 'flatpages/news.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news'] = Post.objects.all()
+        return context
 
 
 class NewsDetail(DetailView):
